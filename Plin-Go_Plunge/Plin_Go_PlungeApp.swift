14180f125caf59844f4146c1efca26801609f7fb
Alexander <oBelyash@yandex.ru>
2025-03-04 18:02:54 +0300

    Initial Commit

---
+++ Plin-Go_Plunge/Plin_Go_PlungeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plin_Go_PlungeApp.swift
+//  Plin-Go_Plunge
+//
+//  Created by Александр Андреев on 04.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Plin_Go_PlungeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
